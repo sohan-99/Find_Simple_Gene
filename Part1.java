e03c8053d8d4bfde718975a0dac3ca081eb4a74a
sohan-99 <sohan75632@gmail.com>
2023-04-13 23:18:26 +0600

    first commit

---
+++ Part1.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package stringsfirstassignments;
+
+/**
+ *
+ * @author Sohan
+ */
+public class Part1 {
+    
+}
