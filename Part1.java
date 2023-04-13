00ee6248c16ee3f1583674d3607cebe9936878a1
sohan-99 <sohan75632@gmail.com>
2023-04-14 00:35:44 +0600

    start_method

--- Part1.java
+++ Part1.java
@@ -4,12 +4,26 @@
  */
 package stringsfirstassignments;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Sohan
  */
 public class Part1 {
-    public static void main(String[]arg){
-    System.out.println("Hellow world");
+  //  public void findSimpleGene(String[] arg) {
+        //Scanner dna = new Scanner(System.in);
+        //String DNA = dna.nextLine();
+       // String dna= "dna";
+       // System.out.println(dna.startsWith("ATG"));
+   // }
+
     
-}}
+
+    public static void main(String[]arg){
+    //System.out.println("Hellow world");
+    //findSimpleGene();
+    String dna= "dna";
+    System.out.println(dna.startsWith("ATG"));
+}
+}
---
+++ String_14.java
@@ -0,0 +1,48 @@
+public class String_14 {
+    public static void main(String[] arg) {
+        String name = "Sohan";
+        // string not changeable
+        // create new obj in string
+        /*
+        String lString=name.toLowerCase();
+        // System.out.println(lString);
+        */
+        /*
+        String uString=name.toUpperCase();
+        // System.out.println(uString);
+        */
+        /*
+        int value=name.length();
+        // System.out.println(value);
+        */
+        /*
+        String nonTrimmedString = " Sohan";
+         System.out.println(nonTrimmedString);
+         System.out.println(nonTrimmedString.trim());
+        */
+        /* 
+         System.out.println(name.substring(4));
+         System.out.println(name.substring(1));
+         System.out.println(name.substring(1,5));
+         */
+       // String dna ="ATG";
+       // System.out.println(dna.startsWith("ATG"));
+        /* System.out.println(name.replace('o','m'));
+        System.out.println(name.replace("oh", "kk"));
+        */
+       /*  System.out.println(name.startsWith("Soh"));
+         System.out.println(name.startsWith("oh"));
+         System.out.println(name.endsWith("an"));
+        System.out.println(name.endsWith("anm"));
+        
+        /*
+        System.out.println(name.charAt(0));
+        System.out.println(name.charAt(1));
+        System.out.println(name.charAt(2));
+        System.out.println(name.charAt(3));
+        System.out.println(name.charAt(4));
+        */
+
+    }
+
+}
